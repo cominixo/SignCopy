2ca3e9b64fced91ece194be8228b836461c77bad
cominixo01 <22600756+cominixo01@users.noreply.github.com>
2020-09-10 22:45:36 -0300

    Initial commit

---
+++ src/main/java/me/cominixo/signcopy/mixin/AbstractSignBlockMixin.java
@@ -0,0 +1,55 @@
+package me.cominixo.signcopy.mixin;
+
+import net.minecraft.block.AbstractSignBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.SignBlockEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractSignBlock.class)
+public class AbstractSignBlockMixin {
+
+    @Inject(method = "onUse", at = @At("HEAD"))
+    public void onSignUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
+        if (player.isSneaking()) {
+            BlockEntity blockEntity = world.getBlockEntity(pos);
+            if (blockEntity instanceof SignBlockEntity) {
+                SignBlockEntity signBlockEntity = (SignBlockEntity) blockEntity;
+
+                // Using an accessor here to get the lines, didn't want to hardcode the number of lines for possible mod compat
+                Text[] lines = ((SignBlockEntityAccessor)signBlockEntity).getText();
+
+                StringBuilder textToCopy = new StringBuilder();
+
+                for (Text text : lines) {
+
+                    String rowString = text.getString();
+
+                    if (!rowString.isEmpty()) {
+                        textToCopy.append(rowString);
+                        textToCopy.append("\n");
+                    }
+
+                }
+
+                MinecraftClient.getInstance().keyboard.setClipboard(textToCopy.toString());
+
+                player.sendMessage(new LiteralText("The text from the sign was copied to your clipboard!").formatted(Formatting.AQUA), true);
+            }
+
+        }
+    }
+}
